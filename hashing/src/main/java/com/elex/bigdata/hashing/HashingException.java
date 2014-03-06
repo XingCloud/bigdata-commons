9afe336a24d138e5a6388cb1d7c13714b31d1d9a
labelnolongerexists <labelnolongerexists@gmail.com>
2014-03-06 13:40:20 +0800

    Add md5 support.

---
+++ hashing/src/main/java/com/elex/bigdata/hashing/HashingException.java
@@ -0,0 +1,21 @@
+package com.elex.bigdata.hashing;
+
+/**
+ * User: Z J Wu Date: 14-3-6 Time: 上午11:53 Package: com.elex.bigdata.hashing
+ */
+public class HashingException extends Exception {
+  public HashingException() {
+  }
+
+  public HashingException(String message) {
+    super(message);
+  }
+
+  public HashingException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public HashingException(Throwable cause) {
+    super(cause);
+  }
+}
